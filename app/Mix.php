6d8d53da363dd152a3790396dd275682741687de
emerge <i.i.babko@gmail.com>
2016-06-17 17:19:25 +0300

    initialize relationships on Track and Mix models

--- app/Mix.php
+++ app/Mix.php
@@ -3,8 +3,12 @@
 namespace Djjeck;
 
 use Illuminate\Database\Eloquent\Model;
+use Track;
 
 class Mix extends Model
 {
-    //
+    public function tracks()
+    {
+        return $this->hasMany(Track::class);
+    }
 }
--- app/Track.php
+++ app/Track.php
@@ -3,8 +3,12 @@
 namespace Djjeck;
 
 use Illuminate\Database\Eloquent\Model;
+use Mix;
 
 class Track extends Model
 {
-    //
+    public function mix()
+    {
+        return $this->belongsTo(Mix::class);
+    }
 }
