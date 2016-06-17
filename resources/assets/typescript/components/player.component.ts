8a34e5c397bc23b29a8082fac2b703b7ea8fc14f
emerge <i.i.babko@gmail.com>
2016-06-17 15:45:44 +0300

    add player template and styles

--- resources/assets/typescript/components/player.component.ts
+++ resources/assets/typescript/components/player.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var jQuery: any;
 
 @Component({
     'selector': 'dj-player',
     'templateUrl': '/templates/components.player'
 })
-export class PlayerComponent {
+export class PlayerComponent implements OnInit {
 
     constructor() {}
+
+    ngOnInit() {
+        
+        jQuery(".jAudio--player").jAudio({
+            playlist: [],
+            defaultAlbum: undefined,
+            defaultArtist: undefined,
+            defaultTrack: 0,
+            autoPlay: false,
+            debug: false
+        });
+
+    }
 }
